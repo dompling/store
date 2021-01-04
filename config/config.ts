f32e32ba260a2c7bb74eca31598e4c1ae2a18f87
dompling <374779789@qq.com>
2021-01-04 16:32:08 +0800

    第一次提交

--- config/config.ts
+++ config/config.ts
@@ -1,34 +1,37 @@
-import path from 'path';
-import {defineConfig} from 'umi';
+import path from "path";
+import { defineConfig } from "umi";
 // @ts-ignore
-import px2rem from 'postcss-plugin-px2rem';
-import routers from './routers';
+import px2rem from "postcss-plugin-px2rem";
+import routers from "./routers";
 
 export default defineConfig({
   nodeModulesTransform: {
-    type: 'none',
+    type: "none"
   },
   alias: {
-    '@': path.resolve(__dirname, 'src'),
+    "@": path.resolve(__dirname, "src")
   },
   routes: routers,
   dynamicImport: {
-    loading: '@/components/PageLoading',
+    loading: "@/components/PageLoading"
   },
+  publicPath: process.env.NODE_ENV === "production"
+    ? "/scriptable-store/"
+    : "/",
   extraPostCSSPlugins: [
     //https://www.npmjs.com/package/postcss-plugin-px2rem
     px2rem({
       rootValue: 256,//开启hd后需要换算：rootValue=designWidth*100/750,此处设计稿为1920，所以1920*100/750=256
       propBlackList: [
-        'border',
-        'border-top',
-        'border-left',
-        'border-right',
-        'border-bottom',
-        'border-radius',
-        'font-size',
+        "border",
+        "border-top",
+        "border-left",
+        "border-right",
+        "border-bottom",
+        "border-radius",
+        "font-size"
       ],//这些属性不需要转换
-      selectorBlackList: ['t_npx'],//以包含t_npx的class不需要转换
-    }),
-  ],
+      selectorBlackList: ["t_npx"]//以包含t_npx的class不需要转换
+    })
+  ]
 });
