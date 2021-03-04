ea5083b08beef76423691d7fff8fe67687b9dd90
dompling <374779789@qq.com>
2021-03-04 13:41:52 +0800

    add:增加图标缓存

--- config/config.ts
+++ config/config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'umi';
 import px2rem from 'postcss-plugin-px2rem';
 import routers from './routers';
 
-const productPath = process.env.NODE_ENV === 'production' ? '/store/' : '/';
+const productPath = process.env.NODE_ENV === 'production' ? '/' : '/';
 // const precomposed = 'https://img.icons8.com/nolan/452/windows-10.png';
 const precomposed = 'https://gitee.com/scriptableJS/store/raw/master/public/icon.png';
 // @ts-ignore
 export default defineConfig({
+  outputPath: 'docs',
   favicon: productPath + 'favicon.ico',
   nodeModulesTransform: {
     type: 'none',
